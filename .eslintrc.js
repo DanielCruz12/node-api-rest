eed6168dabd3fef3db0c9e5a1d3c9f0039f15031
daniel-dubon-dev <danieldubonxd@gmail.com>
2024-06-20 12:55:54 -0600

    --feature jwt token

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,31 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es2021: true
   },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
+    'plugin:prettier/recommended'
   ],
   overrides: [
     {
       env: {
-        node: true,
+        node: true
       },
       files: ['.eslintrc.{js,cjs}'],
       parserOptions: {
-        sourceType: 'script',
-      },
-    },
+        sourceType: 'script'
+      }
+    }
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
-    sourceType: 'module',
+    sourceType: 'module'
   },
   plugins: ['@typescript-eslint', 'prettier'],
-  rules: {},
-};
+  rules: {
+    'no-unused-vars': 'error'
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 // src/index.ts
 const sayHello = (name: string): string => {
+  const h = 1
+
   return `Hello, ${name}!`;
 };
 
